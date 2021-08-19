66364c302f916fc89de9aed371b3d5ac6ff4d9e7
mdkamrulislam-web <md.kamrul.islam@g.bracu.ac.bd>
2021-08-20 01:38:51 +0600

    2nd Commit - 20th August 2021

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,17 +3,55 @@ import 'package:flutter/material.dart';
 const kRoverNameTextStyle = TextStyle(
   fontFamily: 'Montserrat',
   fontSize: 30.0,
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w900,
 );
 
 const kRoverVoyageDateTextStyle = TextStyle(
-  fontFamily: 'Montserrat',
-  fontSize: 15.0,
+  fontSize: 16.0,
 );
 
 const kRoverDetailsTextStyle = TextStyle(
-  fontFamily: 'Montserrat',
+  // fontFamily: 'Montserrat',
+  fontSize: 30.0,
+  height: 1.75,
+  color: Colors.grey,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 1.5,
+);
+
+const kRoverStatusTextStyle = TextStyle(
+  // fontFamily: 'Montserrat',
+  fontSize: 28.0,
+  height: 1.25,
+  color: Colors.green,
+  fontWeight: FontWeight.bold,
+);
+
+const kRoverCameraFullNameTextStyle = TextStyle(
+  // fontFamily: 'Montserrat',
+  fontSize: 20.0,
+  height: 1.25,
+  color: Colors.green,
+  fontWeight: FontWeight.w300,
+);
+
+const kRoverCameraCodeNameTextStyle = TextStyle(
+  // fontFamily: 'Montserrat',
   fontSize: 18.0,
   height: 1.25,
+  color: Colors.green,
+  fontWeight: FontWeight.w300,
+);
+
+const kRoverLaunchLandTextStyle = TextStyle(
+  fontSize: 20.0,
+  height: 1.25,
   color: Colors.grey,
+  fontWeight: FontWeight.bold,
+);
+
+const kRoverLaunchLandDateTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: Colors.grey,
+  fontWeight: FontWeight.bold,
 );
--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
-
+import 'result_view.dart';
 import 'package:flutter/material.dart';
 import 'package:mars_rover_nasa_api_project/API_KEY.dart';
 import 'package:mars_rover_nasa_api_project/constants.dart';
 import 'package:http/http.dart' as http;
 import 'package:mars_rover_nasa_api_project/nasa_mars_rover_api_model.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -14,29 +15,50 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   //Future<http.Response>
 
-  fetchData() async {
+  Future<NASAMarsRoverPhotosAPIModel> fetchData() async {
     //return
     http.Response response = await http.get(Uri.parse(
         'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=$API_KEY'));
 
-    if (response.statusCode == 200) {
-      String data = response.body;
-      print(data);
-      print(response.statusCode);
+    final data =
+        NASAMarsRoverPhotosAPIModel.fromJson(jsonDecode(response.body));
 
-      var fullName = jsonDecode(data)['photos'][0]['camera']['full_name'];
-      print(fullName);
-    } else {
-      print(response.statusCode);
-    }
+    return data;
+
+    // if (response.statusCode == 200) {
+    //   print(response.statusCode);
+    //
+    //   NASAMarsRoverPhotosAPIModel dataModel =
+    //       NASAMarsRoverPhotosAPIModel.fromJson(jsonDecode(response.body));
+    //
+    //   var imageSource = dataModel.photos![0]!.imgSrc;
+    //   //print(imageSource);
+    //   var dateOnEarth = dataModel.photos![0]!.earthDate;
+    //   var landingDate = dataModel.photos![0]!.rover!.landingDate;
+    //   var roverLandingDate = dataModel.photos![0]!.rover!.landingDate;
+    //   var roverLaunchingDate = dataModel.photos![0]!.rover!.launchDate;
+    //   var roverID = dataModel.photos![0]!.rover!.id;
+    //   var roverStatus = dataModel.photos![0]!.rover!.status;
+    //   var cameraID = dataModel.photos![0]!.camera!.id;
+    //   var cameraCode = dataModel.photos![0]!.camera!.name;
+    //   var cameraFullName = dataModel.photos![0]!.camera!.fullName;
+    //   var photoID = dataModel.photos![0]!.id;
+    //
+    //   nasaData = dataModel;
+    //
+    // } else {
+    //   print(response.statusCode);
+    // }
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    fetchData();
+    nasaData = fetchData();
   }
+
+  late Future<NASAMarsRoverPhotosAPIModel> nasaData;
 
   @override
   Widget build(BuildContext context) {
@@ -44,44 +66,26 @@ class _LoadingScreenState extends State<LoadingScreen> {
       appBar: AppBar(
         title: Center(
           child: Text(
-            'Mars Rovers',
+            'CURIOSITY',
           ),
         ),
       ),
-      body: Container(
-        child: ListView(
-          children: [
-            Image(
-              image: NetworkImage(
-                  "https://cdn.britannica.com/q:60/22/206222-050-3F741817/Domestic-feline-tabby-cat.jpg"),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-              child: Text(
-                'Rover Name',
-                style: kRoverNameTextStyle,
+      body: FutureBuilder<NASAMarsRoverPhotosAPIModel>(
+        future: nasaData,
+        builder: (BuildContext context,
+            AsyncSnapshot<NASAMarsRoverPhotosAPIModel> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            NASAMarsRoverPhotosAPIModel dataModel = snapshot.data!;
+            return ResultView(dataModel: dataModel);
+          } else {
+            return Center(
+              child: SpinKitCubeGrid(
+                color: Colors.white,
+                size: 100.0,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
-              child: Text(
-                'Date: 2021-08-09',
-                textAlign: TextAlign.left,
-                style: kRoverVoyageDateTextStyle,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna al.',
-                textAlign: TextAlign.justify,
-                style: kRoverDetailsTextStyle,
-              ),
-            ),
-          ],
-        ),
+            );
+          }
+        },
       ),
     );
   }
